745a8c14dbd51212d13a113309ba9f2bd2049943
pratibha <>
2026-01-15 17:11:38 +0530

    model completed

--- Job_Portal_Backend/src/main/java/com/excelR/Job_Portal_Backend/model/JobApplication.java
+++ Job_Portal_Backend/src/main/java/com/excelR/Job_Portal_Backend/model/JobApplication.java
@@ -1,5 +1,23 @@
 package com.excelR.Job_Portal_Backend.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="")
 public class JobApplication {
-    
+      
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	  private long id;
+	  private long userId;
+	  private long jobId;
+	  private String status;
+//	  it will tell you about where you applied for the job , are you selected or rejected
 }
--- Job_Portal_Backend/src/main/java/com/excelR/Job_Portal_Backend/model/User.java
+++ Job_Portal_Backend/src/main/java/com/excelR/Job_Portal_Backend/model/User.java
@@ -4,6 +4,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import lombok.Data;
+
+
 @Entity
 @Data
 @Table(name="user")
@@ -14,6 +18,7 @@ public class User{
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 
 //	In this the job seeker , the admin of the page and the recruiters information are there
+//	@Entity = first hibernate checks for entity and it says spring that this class should be stored in database
 	
 	private long id;
 	private String name;
