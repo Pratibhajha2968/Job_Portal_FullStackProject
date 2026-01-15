efb19658c47caf214623d8f9f5191004812349e3
Pratibha jha <119747499+Pratibhajha2968@users.noreply.github.com>
2026-01-15 16:32:14 +0530

    Add Lombok and GenerationType to User model

--- Job_Portal_Backend/src/main/java/com/excelR/Job_Portal_Backend/model/User.java
+++ Job_Portal_Backend/src/main/java/com/excelR/Job_Portal_Backend/model/User.java
@@ -4,6 +4,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import lombok.Data;
+
 @Entity
 @Data
 @Table(name="user")
