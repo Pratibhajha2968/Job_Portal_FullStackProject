a75dfcf955cf2a8b556b437cfef0694dc2d1d21d
Pratibha jha <119747499+Pratibhajha2968@users.noreply.github.com>
2026-01-15 15:26:43 +0530

    Add files via upload

---
+++ Job_Portal_Backend/src/main/java/com/excelR/Job_Portal_Backend/model/User.java
@@ -0,0 +1,16 @@
+package com.excelR.Job_Portal_Backend.model;
+
+
+
+public class User{
+	
+//	In this the job seeker , the admin of the page and the recruiters information are there
+	
+	private long id;
+	private String name;
+	private String email;
+	private String password;
+	private String role;
+	
+	
+}
